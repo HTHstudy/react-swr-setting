f39478d6041a7a16424ac8865597c8b580c63fd1
HTHstudy <taehyen11@gmail.com>
2022-01-09 18:57:35 +0900

    material downgrade

--- pages/CustomerService/index.tsx
+++ pages/CustomerService/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import MTable from '@components/MTable';
 
 const CustomerService = () => {
-  return <div>문의 탭</div>;
+  return (
+    <div>
+      문의 탭
+      <MTable />
+    </div>
+  );
 };
 
 export default CustomerService;
